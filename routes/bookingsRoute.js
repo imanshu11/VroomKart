39e7405242f152e95fc1702c6a4e37c9ba782a69
imanshu11 <anshudeo2000@gmail.com>
2022-03-30 23:54:27 +0530

    User Bookings Design

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -50,6 +50,17 @@ router.post("/bookbike", async (req, res) => {
 });
 
 
+router.get("/getallbookings", async(req, res) => {
+
+    try {
+
+        const bookings = await Booking.find()
+        res.send(bookings)
+        
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+});
 
 
 module.exports = router
